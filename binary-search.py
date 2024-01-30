f6dca66d9bca45e84f292ae6f0efc3e9c88233fc
Khushmita Kaur <khushp9894@gmail.com>
2024-01-30 12:37:26 -0800

    add comments

---
+++ binary-search.py
@@ -0,0 +1,6 @@
+# we will compare naive and binary search to show binary search is faster.
+# Naive search: iterate through the entire list and check if it equals the target.
+# if the target is present in the list, return the index. 
+# if not, then return -1
+
+# Binary search: Look for the target in a sorted list using divide and conquer.
